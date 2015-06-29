95d8a11cc4750429e4a1d343187bfaae1965bfdb
Etienne J. Membrives <etienne@membrives.fr>
2015-06-29 20:54:09 +0200

    Fix tripadvisor

--- chrome/js/tripadvisor_place.js
+++ chrome/js/tripadvisor_place.js
@@ -15,12 +15,12 @@
  **/
 
 function getAllPlacesOnPage() {
-    var placeElems = $("#HEADING_GROUP");
+    var placeElems = $("#BODYCON");
     var places = []
     placeElems.each(function(index, placeElem) {
         var name = $(placeElem).find(".heading_name").text().trim();
-        var address = $(placeElem).find(".format_address").text().trim();
-        var resultElem = $(placeElem).find(".heading_wrapper");
+        var address = $(placeElem).find(".map_and_details .format_address").text().trim();
+        var resultElem = $(placeElem).find(".headingWrapper");
         places.push({name: name, address: address, resultElem: resultElem});
     });
     return {places: places, container: placeElems};
